44e05512788d797e478d38d9c7c9d269664973ca
reury-cardoso <reurycardoso7@gmail.com>
2024-09-19 12:12:41 -0300

    :sparkles: feat: finished detail section top

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,12 +1,12 @@
 import "./header.css";
 import iconAdmin from "../../assets/iconAdmin.svg";
-import { Search, Truck } from 'lucide-react';
+import { Search, HeartHandshake } from 'lucide-react';
 
 function Header() {
   return (
     <header>
       <div className="logo">
-        <Truck color="#004FD7"  size={24} />
+        <HeartHandshake color="#004FD7"  size={24} />
         <span>HelpTracking</span>
       </div>
       <div className="secondarySection">
--- src/components/sectionTwoDetails/sectionTwoDetails.jsx
+++ src/components/sectionTwoDetails/sectionTwoDetails.jsx
@@ -1,5 +1,8 @@
 import "./sectionTwoDetails.css";
-import { IdCard, CalendarClock, Zap } from "lucide-react";
+import { Info, Pencil, Trash } from "lucide-react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
 function SectionTwoDetails() {
   return (
     <main id="trackingDetails">
@@ -13,47 +16,81 @@ function SectionTwoDetails() {
           </li>
         </ul>
       </nav>
-
       <section id="details">
-        <div className="itemDetail">
-          <div className="lineDetails">
-            <div>
-            <span className="label">Destinatário:</span>
-            <span className="value"> Maria Carla</span>
+        <div className="itemDetailOne itemDetail">
+          <div className="detailsUp">
+            <div className="detailsTexts">
+              <span>Detalhes</span>
+              <div className="detailsProfile">
+                <img
+                  width={40}
+                  src="https://api.dicebear.com/9.x/initials/svg?seed=Ana Pinho&radius=50&backgroundType=gradientLinear&fontFamily=Arial&fontWeight=600"
+                  alt=""
+                />
+                <div className="profileText">
+                  <span>Ana Pinho</span>
+                  <p>Solicitante #REG103845</p>
+                </div>
+              </div>
             </div>
-            <div>
-              <span className="label">Número:</span>
-              <span className="value"> PDA103920DEV</span>
+            <Info size={40} color="#004FD7" />
+          </div>
+          <div className="detailsDown">
+            <div className="divDetail divDetailData">
+              <span>Dados</span>
+              <div className="typeHelp">
+                <span>Tipo de Ajuda:</span>
+                <p>Dinheiro</p>
+              </div>
+              <div className="createData">
+                <span>Criado em:</span>
+                <p>14/09/2021</p>
+              </div>
+              <div className="updateData">
+                <span>Atualizado em:</span>
+                <p>14/09/2021</p>
+              </div>
+            </div>
+            <div className="divDetail">
+              <span>Status</span>
+              <div style={{ background: "#14C3A4" }} className="statusCard">
+                Aprovado 
+                <Pencil size={20} color="#fff" />
+              </div>
+            </div>
+            <div className="divDetail divDetailData">
+              <span>Endereço</span>
+              <div className="typeHelp">
+                <p>Rua Siqueira Mendes, 190, Centro - Abaetetuba</p>
+              </div>
             </div>
           </div>
-          <IdCard size={40} color="#004FD7" />
         </div>
-        <div className="itemDetail">
-          <div className="lineDetails">
-            <div>
-              <span className="label">Destinatário:</span>
-              <span className="value">Maria Carla</span>
-            </div>
-            <div>
-              <span className="label">Tracking Number:</span>
-              <span className="value">PDA103920DEV</span>
-            </div>
-          </div>
-          <CalendarClock size={40} color="#004FD7" />
+        <div className="itemDetailTwo itemDetail">
+          <MapContainer
+            className="small-map"
+            center={[-1.7252526, -48.8900453]}
+            zoom={13}
+            scrollWheelZoom={false}
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[-1.7252526, -48.8900453]}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+              </Popup>
+            </Marker>
+          </MapContainer>
         </div>
-        <div className="itemDetail">
-          <div className="lineDetails">
-            <div>
-              <span className="label">Tracking Number:</span>
-              <span className="value">PDA103920DEV</span>
-            </div>
-            <div>
-              <span className="label">Tracking Number:</span>
-              <span className="value">PDA103920DEV</span>
-            </div>
-          </div>
-          <Zap size={40} color="#004FD7" />
+        <div className="itemDetailThree itemDetail">
+          <button className="buttonEdit iconCRUD"><Pencil  size={24} color="#fff" /></button>
+          <button className="buttonDelete iconCRUD"><Trash  size={24} color="#fff" /></button>
         </div>
+      </section>
+      <section id="history">
+        
       </section>
     </main>
   );
